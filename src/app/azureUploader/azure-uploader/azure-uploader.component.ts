803dadd62fb02db492a7ad8aa592c7c973e06101
houssamkassab <samokassab_997@outlook.com>
2021-03-29 16:32:31 +0300

    -Second commit

---
+++ src/app/azureUploader/azure-uploader/azure-uploader.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-azure-uploader',
+  templateUrl: './azure-uploader.component.html',
+  styleUrls: ['./azure-uploader.component.scss']
+})
+export class AzureUploaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
